745bafd5e962d0c138438873789e4f37ee330e61
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2022-04-18 09:57:50 +0530

    Create Search a 2D Matrix II.cpp

---
+++ Search a 2D Matrix II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int row = mat.size();
+        int col = mat[0].size();
+        
+        int i=0,j=col-1;
+        while(i < row && j >= 0){
+            if(mat[i][j] == target) return true;
+            
+            else if(mat[i][j] < target) i++;
+            else j--;
+        }
+        return false;
+    }
+};
