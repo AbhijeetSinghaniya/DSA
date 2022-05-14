3ec9900e01cf3ea135cde156bdc767999566c9fd
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2022-05-14 20:35:51 +0530

    Update Delete and Earn.cpp

--- Dynamic Programming/Delete and Earn.cpp
+++ Dynamic Programming/Delete and Earn.cpp
@@ -16,3 +16,21 @@ public:
         return max(take, skip);
     }
 };
+
+//M - II : same logic, just different way of writing code
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        
+        int mx=*max_element(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int> freq(mx+1,0),dp(mx+1,0);
+        for(int i=0; i<n; i++) freq[nums[i]]++;
+        dp[0]=0; dp[1]=freq[1]; dp[2]=max(dp[1],freq[2]*2);
+        for(int i=3; i<=mx; i++){
+            dp[i]=max(dp[i-1],dp[i-2]+freq[i]*i);
+        }
+        return dp[mx];
+    }
+};
