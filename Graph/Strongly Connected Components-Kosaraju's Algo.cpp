7b3281d001bddbd5d2f2dc105b954ce4f2759106
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2023-12-19 16:56:17 +0530

    Create Strongly Connected Components-Kosaraju's Algo.cpp
    https://practice.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=strongly-connected-components-kosarajus-algo

---
+++ Graph/Strongly Connected Components-Kosaraju's Algo.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    void dfs(int node,vector<vector<int>> adj,vector<int>& vis,stack<int>& st){
+        vis[node] = 1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,vis,st);
+            }
+        }
+        st.push(node);
+    }
+    void dfsT(int node,vector<int> adjT[],vector<int>& vis){
+        vis[node] = 1;
+        for(auto it : adjT[node]){
+            if(!vis[it]){
+                dfsT(it,adjT,vis);
+            }
+        }
+    }
+    
+	public:
+    int kosaraju(int V, vector<vector<int>>& adj)
+    {
+        vector<int> vis(V,0);
+        stack<int> st;
+        for(int i=0;i<V;i++){
+            if(!vis[i]) dfs(i,adj,vis,st);
+        }
+        
+        vector<int> adjT[V];    
+        for(int i=0;i<V;i++){
+            vis[i] = 0;
+            for(auto it : adj[i]){
+                // i -> it TO it -> i
+                adjT[it].push_back(i);
+            }
+        }
+        
+        int scc = 0;
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();            
+            if(!vis[node]){
+                scc++;
+                dfsT(node,adjT,vis);
+            }
+        }
+        return scc;
+    }
+};
