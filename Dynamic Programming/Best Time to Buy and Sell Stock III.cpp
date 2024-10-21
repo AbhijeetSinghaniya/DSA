3d4fb2835231ded6a282568fbc4391c6f5bcd8c2
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2024-10-22 00:57:35 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
+++ Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
@@ -1,3 +1,5 @@
+//M-I : Recursion + Memoization
+
 class Solution {
 public:
     int solve(int ind, int buy, int cap, vector<int>& prices, int n, 
@@ -22,5 +24,31 @@ public:
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         int ans = solve(0,1,2,prices,n,dp);
         return ans;    
+    }
+};
+
+//M-II : Tabulation
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for(int ind=n-1;ind>=0;ind--) {
+            for(int buy=0;buy<=1;buy++) {
+                for(int cap=1;cap<=2;cap++) {
+                    if(buy == 1) {
+                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
+                                                0 + dp[ind+1][1][cap]);
+                    }
+                    else {
+                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
+                                                0 + dp[ind+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
