9e54a4533305d7695b6fc8ceaa88d7bcc7becf8e
Abhijeet Singhaniya <2018abhijeet@gmail.com>
2024-10-22 00:59:24 +0530

    Create Best Time to Buy and Sell Stock IV.cpp

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,54 @@
+//M-I : Recursion + Memoization
+
+class Solution {
+public:
+    int solve(int ind, int buy, int cap, vector<int>& prices, int n, 
+                vector<vector<vector<int>>>& dp) {
+        if(ind == n || cap == 0) return 0;
+
+        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
+
+        int profit = 0;
+        if(buy) {
+            profit = max(-prices[ind] + solve(ind+1,0,cap,prices,n,dp),
+                         0 + solve(ind+1,1,cap,prices,n,dp));
+        } else {
+            profit = max(prices[ind] + solve(ind+1,1,cap-1,prices,n,dp),
+                         0 + solve(ind+1,0,cap,prices,n,dp));
+        }
+        return dp[ind][buy][cap] = profit;
+    }
+
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        int ans = solve(0,1,k,prices,n,dp);
+        return ans;    
+    }
+};
+
+//M-II : Tabulation
+
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        for(int ind=n-1;ind>=0;ind--) {
+            for(int buy=0;buy<=1;buy++) {
+                for(int cap=1;cap<=k;cap++) {
+                    if(buy == 1) {
+                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
+                                                0 + dp[ind+1][1][cap]);
+                    }
+                    else {
+                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
+                                                0 + dp[ind+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+};
